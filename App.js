f3ec00d66989ab3f500faee017d9d0c75592a5d7
turjoy-real <sahaturjoy@gmail.com>
2021-11-30 09:56:31 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import Basic from "./components/basicSwipe";
+// import SwipeValueBasedUi from "./components/swipeUI";
+import SwipeToDelete from "./components/swipeToDelete";
+// import SectionList from "./examples/sectionlist";
+// import PerRowConfig from "./examples/per_row_config";
+// import StandloneSwipeRow from "./examples/standalone_swipe_row";
+// import SwipeToDelete from "./examples/swipe_to_delete";
+// import SwipeValueBasedUi from "./examples/swipe_value_based_ui";
+
+const componentMap = {
+  Basic,
+  // SectionList,
+  // PerRowConfig,
+  // StandloneSwipeRow,
+  SwipeToDelete,
+  // SwipeValueBasedUi,
+};
 
 export default function App() {
+  const [mode, setMode] = useState("Basic");
+
+  const renderExample = () => {
+    const Component = componentMap[mode];
+    return <Component />;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.switchContainer}>
+        {Object.keys(componentMap).map((type) => (
+          <TouchableOpacity
+            key={type}
+            style={[
+              styles.switch,
+              {
+                backgroundColor: mode === type ? "grey" : "white",
+              },
+            ]}
+            onPress={() => setMode(type)}
+          >
+            <Text>{type}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      {renderExample()}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "white",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  switchContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginVertical: 50,
+    flexWrap: "wrap",
+  },
+  switch: {
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "black",
+    marginVertical: 2,
+    paddingVertical: 10,
+    width: Dimensions.get("window").width / 3,
   },
 });
